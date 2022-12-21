2f4e4e13cb3f532a68bc8622920e181ccbdbd1dc
WebWorksAfrica <tinashe@webworks.co.zw>
2022-12-21 03:02:32 +0200

    Changes to be committed: 	modified:   src/environments/environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,7 @@ export const environment = {
     production: false,
     baseUrl: 'https://wadeamg.my.salesforce.com/services/data/v54.0',
     loginUrl: 'https://gigi.herokuapp.com/services/oauth2/token?',
+    // eslint-disable-next-line max-len
     creds:'client_id=3MVG9JZ_r.QzrS7iBlm11gVzyscKtVi2mIiWfpwhmcneG7K0MwhbqFshlxd0hR92Jbbf1OF1HArBSpH1oe9yg&client_secret=D188BD0F3054C061602E5861E276F4F531464B090519859320B130F86BBC35C8&grant_type=password&username=office@wadeamg.com&password=GIUSA2022!',
 
 };
